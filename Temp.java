e1d787b3ca4fc8306afeeac4374a973d631348ae
jaeseonglee <50550186+jaeseonglee@users.noreply.github.com>
2019-10-06 21:48:43 +0900

    Add files via upload

---
+++ Temp.java
@@ -0,0 +1,15 @@
+/** 숙제 1번 임시 코드
+ *  작성자 : 20165153 이재성
+ *  작성일 : 2019.10.06
+ *	프로그램 내용 : 자바에서의 자료형의 크기를 구하는 프로그램 
+ */
+public class Temp {
+	public static void main(String [] args) {
+		System.out.println("char: " + Character.BYTES);
+		System.out.println("int: " + Integer.BYTES);
+		System.out.println("long: " + Long.BYTES);
+		
+		System.out.println("float: " + Float.BYTES);
+		System.out.println("double: " + Double.BYTES);
+	}
+}
